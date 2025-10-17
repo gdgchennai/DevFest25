fda84f1daabe3228f1bdf1b92d44689d3f374f16
Spikey Sanju <spikeysanju98@gmail.com>
2025-10-17 12:15:43 +0530

    remove responsive menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -55,8 +55,9 @@
 
 	<!-- Additional SEO Meta Tags -->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="theme-color" content="#ffffff" />
 	<meta name="apple-mobile-web-app-capable" content="yes" />
-	<meta name="apple-mobile-web-app-status-bar-style" content="default" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 	<meta name="apple-mobile-web-app-title" content="DevFest Chennai 2025" />
 
 	<!-- Canonical URL -->
